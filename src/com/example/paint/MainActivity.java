e609e60ee840fe04add7c93dde8bb379b83e0b61
ITLAB <ITLAB@IT>
2017-02-07 16:07:17 +0530

    third commit

--- src/com/example/paint/MainActivity.java
+++ src/com/example/paint/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.paint;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -21,8 +23,35 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button1);
+        b2=(Button)findViewById(R.id.button2);
+        b3=(Button)findViewById(R.id.button3);
+        e1=(EditText)findViewById(R.id.editText1);
+        e2=(EditText)findViewById(R.id.editText2);
+        e3=(EditText)findViewById(R.id.editText3);
+        e4=(EditText)findViewById(R.id.editText4);
+   
+        b1.setOnClickListener(new View.OnClickListener() {
+        	     		
+        	 			@Override
+        	 			public void onClick(View arg0) {
+        	 				// TODO Auto-generated method stub
+        	 				
+        	 				//String data=textInput.getText().toString();
+        	 				
+        	 				Toast msg=Toast.makeText(getApplicationContext(),"Please wait...", Toast.LENGTH_LONG);
+        	 				msg.show();
+        	 				
+        	 				Intent i=new Intent(MainActivity.this,SecondActivity.class);
+        	 				//i.putExtra("Username", data);
+        	 				startActivity(i); 
+        	 			}
+        	 			
+        	 		});
+        	      
     }
-} 
+}
+ 
     
 
 /*import android.app.Activity;
---
+++ src/com/example/paint/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.paint;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity{
+	
+//TextView result;
+	
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+		//result=(TextView)findViewById(R.id.heee);
+		//result.setText("Test");
+		//Intent i=getIntent();
+		//String data_main_activity=i.getStringExtra("Username");
+		//result.setText(data_main_activity);
+
+	}
+
+	
+}
