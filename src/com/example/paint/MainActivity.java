f708822d44841007999b32a49e9369a6277027e3
ITLAB <ITLAB@IT>
2017-02-07 15:32:05 +0530

    second commit

--- src/com/example/paint/MainActivity.java
+++ src/com/example/paint/MainActivity.java
@@ -1,35 +1,69 @@
 package com.example.paint;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
-
+	
+	Button b1,b2,b3;
+	EditText e1,e2,e3,e4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+} 
+    
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+/*import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+ 
+public class MainActivity extends Activity {
+	
+	MyView graphics;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		graphics = new MyView(this);
+		setContentView(graphics);
+	}
+	private class MyView extends View{
+		public MyView(Context context){
+			super(context);
+		}
+	
+	protected void onDraw(Canvas canvas){
+		super.onDraw(canvas);
+		Paint paint = new Paint();
+		paint.setStyle(Paint.Style.FILL);
+		paint.setColor(Color.BLACK);
+		canvas.drawPaint(paint);
+		paint.setAntiAlias(false);
+		paint.setColor(Color.RED);
+		canvas.drawCircle(60,90,30,paint);
+		paint.setAntiAlias(true);
+		paint.setColor(Color.BLUE);
+		canvas.drawCircle(45,65,20,paint);*/
+		
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-}
+
+
+		
+ 
